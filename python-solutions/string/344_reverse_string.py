2473c3213c976a549ef62e5882a897b9cbe6f4db
gvanvari <gvanvari@gmail.com>
2020-04-07 14:18:39 -0400

    solved 2 months back

---
+++ python-solutions/string/344_reverse_string.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        j = len(s)-1
+        for i in range(int(len(s)/2)) :
+            temp = s[i]
+            s[i] = s[j]
+            s[j] = temp
+            j = j-1
---
+++ python-solutions/string/345_reverse_vowels_string.py
@@ -0,0 +1,21 @@
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        c = list(s)  # string to list
+        j = len(s) - 1
+        i = 0
+        vowel = ["a", "e", "i", "o", "u", 'A', 'E', 'I', 'O', 'U']
+
+        while (i < j):
+            if c[i] in vowel and c[j] in vowel:
+                c[i], c[j] = c[j], c[i]  # tuple swap
+                j = j - 1
+                i = i + 1
+            elif c[i] in vowel:
+                j = j - 1
+            elif c[j] in vowel:
+                i = i + 1
+            else:
+                j = j - 1
+                i = i + 1
+
+        return ''.join(c)
